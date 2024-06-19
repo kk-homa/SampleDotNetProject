32da5928639cb63a2104acc43e6e6ec3055cd211
kk <kirill.kostenkov@homagames.com>
2024-06-19 17:01:35 +0200

    Add code that violates code style

---
+++ ConsoleApp/FooClass.cs
@@ -0,0 +1,6 @@
+namespace ConsoleApp;
+
+public class FooClass
+{
+    public int _someVar = 10;
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ConsoleApp;
+
 Console.WriteLine("Hello, World!");
+var b = new FooClass();
