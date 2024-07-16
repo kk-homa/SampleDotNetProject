da449c7798e4db76cac5ca2f3567f44bd5d5dd03
kk <kirill.kostenkov@homagames.com>
2024-07-16 11:54:01 +0200

    Reapply code violations

--- FooLib/FooClass.cs
+++ FooLib/FooClass.cs
@@ -1,6 +1,9 @@
-﻿namespace SampleDotNetProject;
+﻿namespace ConsoleApp;
 
 public class FooClass
 {
-
+    public int _someVar = 10;
+    public int _someVar1 = 10;
+    private string SomeVar1 = "bar";
+    private string SomeVar2 = "bar";
 }
